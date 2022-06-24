6aa5caf36db4a8a990acf868f1628486cef6d1de
rololevy <84113089+rololevy@users.noreply.github.com>
2022-06-24 00:37:00 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -4,3 +4,4 @@
 void cerditos();
 
 #endif // FUNCIONES_H_INCLUDED
+void empiezo_estructura_juego(string jugador1[], string jugador2[], int& acu_trufas_jug1, acu_trufas_jug2, bool& jugador_empieza, int& lanzar_mejor_dado1, int& lanzar mejor_dado2, int& oinks_jug1, int& oinks2);
